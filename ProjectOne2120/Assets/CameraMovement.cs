1b8f7dd7f6a0ab25215f1959e5e41045bebf3286
Edgar <edgar.alejandro@mail.citytech.cuny.edu>
2019-09-23 11:46:01 -0400

    First Draft Of Project
    This is the project file that was just added

---
+++ ProjectOne2120/Assets/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private Vector3 lookAngle;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0,0 , 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1 );
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetAxis("Mouse X") != 0) 
+        {
+            lookAngle.y += Input.GetAxis("Mouse Y");
+        }
+
+        transform.rotation = Quaternion.Euler(lookAngle);
+    }
+}
---
+++ ProjectOne2120/Assets/PlatformLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLoop : MonoBehaviour
+{
+    public GameObject WallBuilder;
+
+    void Update()
+    {
+        BuildAWallAtY(-1);
+    }
+    // Start is called before the first frame update
+    void BuildAWallAtY(float y)
+    {
+        for (int xPos = 0; xPos < 40; xPos++)
+        {
+            for (int zPos = 0; zPos < 10; zPos++)
+            {
+                Instantiate(WallBuilder, new Vector3(xPos - 10, y, zPos), Quaternion.identity);
+            }
+        }
+    }
+}
