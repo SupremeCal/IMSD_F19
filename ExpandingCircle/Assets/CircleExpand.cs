42161185e45b8d66ecb4194f27e178441f4f8462
Ekemini <3k3m1n1@gmail.com>
2019-11-20 11:38:59 -0500

    Edited Script

--- ExpandingCircle/Assets/CircleExpand.cs
+++ ExpandingCircle/Assets/CircleExpand.cs
@@ -4,36 +4,40 @@ using UnityEngine;
 
 public class CircleExpand : MonoBehaviour
 {
-    Transform[] circles = new Transform[2];
+    GameObject[] circles;
     public GameObject boxPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 2; i++)
+        circles = new GameObject[3];
+        for (int i = 0; i < 3; i++)
         {
-            GameObject g = Instantiate(boxPrefab, new Vector3(-4 + 2 * i, 0, 0), Quaternion.identity);
-            circles[i] = g.transform;
+            circles[i]= Instantiate(boxPrefab, new Vector3(-2 + (2 * i), 0, 0), Quaternion.identity); 
         }
     }
     // Update is called once per frame
     void Update()
     {
+        circles[0].GetComponent<SizeChangeScript>().MakeMyBool(Input.GetKey(KeyCode.Q));
+        circles[1].GetComponent<SizeChangeScript>().MakeMyBool(Input.GetKey(KeyCode.W));
+        circles[2].GetComponent<SizeChangeScript>().MakeMyBool(Input.GetKey(KeyCode.P));
+
         //ExpandTheSphere(circles[0], Input.GetKey(KeyCode.A));
     }
-    void ExpandTheSphere(Transform box ,float timer, float x, float y, float z, bool size)
-    {
-        x = 1;
-        y = 1;
-        z = 1;
-        timer += Time.deltaTime;
+        //void ExpandTheSphere(Transform box ,float timer, float x, float y, float z, bool size)
+        //{
+        //    x = 1;
+        //    y = 1;
+        //    z = 1;
+        //    timer += Time.deltaTime;
         
-        if (size)
-        {
-            transform.localScale += new Vector3(x, y, z);
-        }
-        else
-        {
-            transform.localScale += new Vector3(x - timer, y - timer, z - timer);
-        }
-    }
+        //    if (size)
+        //    {
+        //        transform.localScale += new Vector3(x, y, z);
+        //    }
+        //    else
+        //    {
+        //        transform.localScale += new Vector3(x - timer, y - timer, z - timer);
+        //    }
+        //}
 }
