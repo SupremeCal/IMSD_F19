f0cf1630148bb6ce6ba4b2e790c9255435a479d9
Edgar Alejandro <edgar.alejandro@mail.citytech.cuny.edu>
2019-11-19 22:17:23 -0500

    expand circle HW

---
+++ ExpandingCircle/Assets/CircleExpand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleExpand : MonoBehaviour
+{
+    Transform[] circles = new Transform[2];
+    public GameObject boxPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject g = Instantiate(boxPrefab, new Vector3(-4 + 2 * i, 0, 0), Quaternion.identity);
+            circles[i] = g.transform;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //ExpandTheSphere(circles[0], Input.GetKey(KeyCode.A));
+    }
+    void ExpandTheSphere(Transform box ,float timer, float x, float y, float z, bool size)
+    {
+        x = 1;
+        y = 1;
+        z = 1;
+        timer += Time.deltaTime;
+        
+        if (size)
+        {
+            transform.localScale += new Vector3(x, y, z);
+        }
+        else
+        {
+            transform.localScale += new Vector3(x - timer, y - timer, z - timer);
+        }
+    }
+}
---
+++ ProjectOne2120/Assets/FlashLightSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSpawner : MonoBehaviour
+{
+    public GameObject flashlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(flashlight, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
